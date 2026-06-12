9230cc10a0758d5cfdf6031b615aaa6aba7b9b48
rahuldevopslearn <131506642+rahuldevopslearn@users.noreply.github.com>
2026-06-12 14:21:20 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "dev-rahul19-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     use_lockfile = true
